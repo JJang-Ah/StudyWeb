5bfe680c088426094df5ced7d900f1af34ea5430
JJang-Ah <tw5hyun2002@gmail.com>
2022-03-14 18:24:10 +0900

    22.03.14

---
+++ workspace_js/js0314/js/basic01.js
@@ -0,0 +1,13 @@
+
+//2. 외부 자바스크립트 작성
+//
+
+
+let t1 = "3월도 벌써 중반입니다.";
+let t2 = "공부에는 노력외에 왕도는 없습니다."
+function welcom2() {
+    document.write(t1 + "<br>");
+    document.write(t2);
+}
+
+welcom2();
